236d2f6bd6f3b959a125c6b704010d549a6fbc9f
wilfried KONE <83975659+wilfriedkone@users.noreply.github.com>
2025-02-24 14:07:26 +0100

    return item

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-left h-20">
+        <div className="flex justify-between items-center h-20">
           {/* Logo */}
           <Link to="/" className="flex-shrink-0">
             <span className="text-2xl font-bold text-primary-600">QUALICOM</span>
